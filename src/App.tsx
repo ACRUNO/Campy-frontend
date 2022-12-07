16de87d6da1655fd814a60d3d82451949caea7f2
Joaquin Delgado <jotadelgado96@gmail.com>
2022-12-07 13:15:49 -0300

    first push

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home/Home'
+
+function App() {
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path = "/" element={ <Home/> } />
+        <Route path = "/booking" />
+        <Route path = "/booking/camping/:id" />
+        <Route path = "/login" />
+        <Route path = "/map" />
+        <Route path = "/blog" />
+        <Route path = "/create" />
+        <Route path = "/about" />
+      </Routes>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/reducer/index.ts
@@ -0,0 +1,18 @@
+
+
+const initialState : any = {
+    inicio: 0
+};
+
+
+function rootReducer(state : any = initialState, action : any): any {
+    return initialState;
+}
+
+
+
+
+
+
+
+export default rootReducer;
