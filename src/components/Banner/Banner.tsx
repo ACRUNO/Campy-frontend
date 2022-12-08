39fa45681203ea718c91565ed013f562f0352987
augusto2304 <augusto.alvarez2304@yahoo.com>
2022-12-08 15:19:31 -0300

    Cards Complete

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Box } from '@mui/material'
+import { height, maxHeight, maxWidth } from "@mui/system";
+import s from './Banner.module.css'
 
 
 export default function Banner() {
+    const banner:string ="https://www.argentinosxargentina.ar/media/sticky/1920x830/zc/slide_2.jpg" 
     return (
         <>
-       
+    
+       <Box
+                            component="img"
+                            className={s.banner}
+                            alt="Paisaje"
+                            src={banner}
+                        />
         </>
 
     )
