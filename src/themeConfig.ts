9f9921730ad55cb38504ac70686ab70293482a2f
Joaquin Delgado <jotadelgado96@gmail.com>
2022-12-08 15:16:37 -0300

    Login y Nav

--- src/themeConfig.ts
+++ src/themeConfig.ts
@@ -11,6 +11,11 @@ const theme = createTheme({
             main: '#5f8d4e',
             dark: '#285430',
             light: '#A4BE7B'
+        },
+        info: {
+            main: '#0A0E06',
+            dark: '#070C03',
+            light: '#494D44'
         }
     },
     typography: {
