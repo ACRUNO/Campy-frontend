43589ae5955573542bcd8e7ddf447150a1817457
Virignia <virginia.juarez1996@gmail.com>
2022-12-11 15:55:38 -0300

    paginado listo

--- src/components/Paginado.tsx
+++ src/components/Paginado.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Pagination , Stack, Box} from "@mui/material";
 
-export default function Paginado() {
-    return (
-        <h1>Esto es un paginado</h1>
-    )
+type Props = {
+    campingsxPage:number,
+    allCampings: number,
+    currentPage: number,
+    setCurrentPage: (value: React.SetStateAction<number>) => void
+  }
+
+export default function Paginado (props: Props){
+
+    const handleChange=(e:React.ChangeEvent<unknown>,page:number)=>{
+        props.setCurrentPage(page)
+    }
+return (
+    <Box sx={{display:"flex", justifyContent:"center", mb:1, mt:1}}>
+    <Stack  spacing={2}>
+      <Pagination count={Math.ceil(props.allCampings/props.campingsxPage)} color="secondary" onChange={handleChange}/>
+    </Stack>
+    </Box>
+)
 }
