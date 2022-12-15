4d27d6e627ea9e0273b8cd6a03dbe372538225f2
Agustin Romero <agusromero0815@gmail.com>
2022-12-15 13:54:54 -0300

    Modificando el signup del navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,30 @@
 import { Toolbar, AppBar, Button, Typography, Box, Link } from "@mui/material";
 import styles from './NavBar.module.css'
-import LoginIcon from '@mui/icons-material/Login';
+import {Login as LoginIcon, Logout as LogoutIcon} from '@mui/icons-material';
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import { ROJO, VERDE } from "../helpers/colors";
+import { useAuth0 } from "@auth0/auth0-react";
+import { logoutUser } from "../../actions";
 
 const pages: string[] = ['blog', 'booking', 'map', "create"];
 const logo : string = "https://res.cloudinary.com/pfcampy/image/upload/v1670466096/logo_CAMPY_rjsp9a.png"
 
 export default function NavBar() {
 
-    return (
+    const dispatch: AppDispatch = useDispatch();
+    const user = useSelector((state: RootState) => state.user);
+    const { logout, isAuthenticated } = useAuth0();
+
+    const handlerLogoutUser = (e: any) => {
+        localStorage.removeItem('token')
+        
+        if(isAuthenticated) return logout();
+
+        dispatch(logoutUser());
+    } 
+
+    return (    
         <>
             <AppBar className={styles.appbar} component='nav' position="fixed">
                 <Toolbar>
@@ -54,15 +71,24 @@ export default function NavBar() {
                             mr: "1%"
                         }}
                     >
-                        <Link href='/login'>
+                        <Link href='/login' onClick={handlerLogoutUser}>
                             <Button
                                 className={styles.login}
                                 variant='text'
                                 color="info">
                                 {/* <Typography component='div'>Log in</Typography> */}
-                                <LoginIcon
+                                {
+                                    user ?
+                                    <LogoutIcon 
                                     fontSize="large"
-                                />
+                                    sx={{fill: ROJO}}
+                                     />
+                                    :
+                                    <LoginIcon
+                                    fontSize="large"
+                                    sx={{fill: VERDE}}
+                                     />
+                                }
                             </Button>
                         </Link>
                     </Box>
