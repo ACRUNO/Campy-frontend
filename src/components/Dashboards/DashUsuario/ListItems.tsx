4e99ee91633704f4a53c41df004eae60a70b3830
Agustin Romero <agusromero0815@gmail.com>
2022-12-16 22:46:16 -0300

    Desarrollando estructura del dashboard usuario

--- src/components/Dashboards/DashUsuario/ListItems.tsx
+++ src/components/Dashboards/DashUsuario/ListItems.tsx
@@ -3,28 +3,10 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import PlaceIcon from '@mui/icons-material/Place';
-import HolidayVillageIcon from '@mui/icons-material/HolidayVillage';
-import { Link, Navigate } from "react-router-dom";
-import ListItem from '@mui/material/ListItem';
-import {Collapse} from '@mui/material'
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import {useState} from "react";
 import ListAltIcon from '@mui/icons-material/ListAlt';
-import EditIcon from '@mui/icons-material/Edit';
-import BungalowIcon from '@mui/icons-material/Bungalow';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { VERDE_CLARO, VERDE_OSCURO } from '../../helpers/colors';
 
 type Props = {
   setFavoritos: (value: React.SetStateAction<boolean>) => void,
@@ -51,24 +33,25 @@ export default function ListItems(props:Props){
     props.setMisdatos(true)
   }
 
+
   return(
   <React.Fragment>
     <List>
     <ListItemButton onClick={handleClickMisdatos}>
       <ListItemIcon>
-        <AccountCircleIcon/>
+        <AccountCircleIcon sx={{fill: VERDE_CLARO }} />
       </ListItemIcon>
       <ListItemText primary="Mis Datos" />
     </ListItemButton>
     <ListItemButton onClick={handleClickFavoritos}>
       <ListItemIcon>
-        <FavoriteIcon/>
+        <FavoriteIcon  sx={{fill: VERDE_CLARO }} />
       </ListItemIcon>
       <ListItemText primary="Mis Favoritos" />
     </ListItemButton>
     <ListItemButton onClick={handleClickReservas}>
       <ListItemIcon>
-        <ListAltIcon/>
+        <ListAltIcon  sx={{fill: VERDE_CLARO }} />
       </ListItemIcon>
       <ListItemText primary="Mis Reservas" />
     </ListItemButton>
