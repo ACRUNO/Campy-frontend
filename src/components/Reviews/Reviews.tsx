d6e03da2d5cabd44f50e28336a86565255e76c55
ACRUNO <jotadelgado96@gmail.com>
2022-12-26 19:42:14 -0300

    Modificacion Reviews, Mapa de Creacion y favoritos

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Rating, TextField, Typography } from "@mui/material";
+import { Backdrop, Box, Button, Fade, Grid, Modal, Rating, TextField, Typography } from "@mui/material";
 import { borderColor, width } from "@mui/system";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,7 +8,17 @@ import { AppDispatch, RootState } from "../../store";
 
 
 
-
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '40%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 
 export function Reviews() {
@@ -16,6 +26,9 @@ export function Reviews() {
     const dispatch: AppDispatch = useDispatch()
     const params = useParams()
     const reviews = useSelector((state: RootState) => state.reviews);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     useEffect(() => {
         dispatch(getCampingReviews(params.id));
@@ -25,39 +38,60 @@ export function Reviews() {
 
 
     return (
-        <Box mt='1%' display='flex' sx={{ width: '100%' }}>
-            <Box display='flex' flexDirection='column' justifyContent='flex-start' p='3rem' sx={{ width: '100%' }}>
-                <Box pb='1rem'>
-                    <Typography
-                        variant="h2"
-                        sx={{ textDecorationLine:"underline"}}
-                    >
-                        Reseñas:
-                    </Typography>
-                </Box>
-                <Box alignItems='left' sx={{ width: '70%' }}>
-                    {reviews?.map((reviews: { id_camping: number, puntaje: number, username: string, fecha: string, comentario: string }) => {
-                        return (
-                            <Box display='flex' justifyContent='space-between'>
-                                <Box >
-                                    <Typography variant="h4">
-                                        {reviews.username}
-                                    </Typography>
-                                    <Box>
-                                        {/* <Typography component="legend">Read only</Typography> */}
-                                        <Rating name="read-only" value={reviews.puntaje} readOnly />
-                                    </Box>
-                                </Box>
-                                <Box display='flex' alignItems='center'>
-                                    <Typography variant="body1">
-                                        {reviews.comentario}
-                                    </Typography>
-                                </Box>
+        <div>
+            <Button onClick={handleOpen} color='secondary' variant='contained'>Mostrar Reseñas</Button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <Box mt='1%' display='flex' sx={style}>
+                        <Box display='flex' flexDirection='column' justifyContent='flex-start' p='2rem' sx={{ width: '100%' }}>
+                            <Box pb='3rem' alignItems='center'>
+                                <Typography
+                                    variant="h2"
+                                    sx={{ textDecorationLine: "underline" }}
+                                    textAlign='center'
+                                >
+                                    Reseñas:
+                                </Typography>
                             </Box>
-                        )
-                    })}
-                </Box>
-            </Box>
-        </Box>
+                            <Box alignItems='left' sx={{ width: '100%' }}>
+                                {reviews?.map((reviews: { id_camping: number, puntaje: number, username: string, fecha: string, comentario: string }) => {
+                                    return (
+                                        
+                                        <Box >
+                                            <Box display='flex' justifyContent='space-between'>
+                                                <Typography variant="h4">
+                                                    {reviews.username}
+                                                </Typography>
+                                                <Box>
+                                                    {/* <Typography component="legend">Read only</Typography> */}
+                                                    <Rating name="read-only" value={reviews.puntaje} readOnly />
+                                                </Box>
+                                            </Box>
+                                            <Box display='flex' alignItems='center'>
+                                                <Typography variant="body1">
+                                                    {reviews.comentario}
+                                                </Typography>
+                                            </Box>
+                                            <hr></hr>
+                                        </Box>
+                                        
+                                    )
+                                })}
+                            </Box>
+                        </Box>
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
     )
 }
