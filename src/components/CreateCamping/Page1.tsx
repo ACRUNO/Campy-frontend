6e8d18308bb0985c670468e118e737b6c4e012fb
ACRUNO <jotadelgado96@gmail.com>
2022-12-28 12:56:29 -0300

    Modificacion Create Camping

--- src/components/CreateCamping/Page1.tsx
+++ src/components/CreateCamping/Page1.tsx
@@ -1,17 +1,13 @@
-import React, { ChangeEvent, ChangeEventHandler } from 'react';
+import React, { ChangeEvent } from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from '../../store/index';
-import { getProvincias, getLocalidades, getCampingsProvincias, getCampingsLocalidades, getAllCategorias } from "../../actions";
+import { getProvincias, getLocalidades, getAllCategorias } from "../../actions";
 import { SelectChangeEvent, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import { setUncaughtExceptionCaptureCallback } from 'process';
 import { Inputs } from './CreateCamping';
-import { Wrapper } from '@googlemaps/react-wrapper';
 import MapCreate from './CreateMap';
 
 interface InputProps {
@@ -33,7 +29,6 @@ export default function Page1({ setInput, input }: InputProps) {
   }, [dispatch]);
 
   const [provincia, setProvincia] = useState<number>(0);
-  const [categoria, setCategoria] = useState<number>(0);
 
 
   const handleChangeProvincia = (e: SelectChangeEvent) => {
@@ -50,7 +45,6 @@ export default function Page1({ setInput, input }: InputProps) {
 
   const handleChangeCategoria = (e: SelectChangeEvent) => {
     e.preventDefault();
-    setCategoria(Number(e.target.value) as number);
     setInput((inputs: any) => {
       return {
         ...inputs,
@@ -69,7 +63,6 @@ export default function Page1({ setInput, input }: InputProps) {
     })
   };
 
-
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     e.preventDefault();
     setInput((inputs: Inputs) => {
@@ -79,6 +72,7 @@ export default function Page1({ setInput, input }: InputProps) {
       }
     })
   };
+
 
   return (
     <React.Fragment>
@@ -117,36 +111,11 @@ export default function Page1({ setInput, input }: InputProps) {
             onChange={handleChangeInput}
           />
         </Grid>
-        {/* <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            value={input.direccion}
-            id="Dirección"
-            label="Dirección"
-            name="direccion"
-            autoComplete="shipping address-line1"
-            color='secondary'
-            variant="standard"
-            onChange={handleChangeInput}
-          />
-        </Grid> */}
-        <Grid item justifyContent="center" alignContent="center" sx={{ mt: 1, mb: 1}}>
-        {/* <Wrapper apiKey='AIzaSyChcClmha8e-qVgQpXurFMDX0X57--Nqh8' > */}
-          <MapCreate setInput ={setInput}
-                      input={input}/>
-        {/* </Wrapper> */}
+        <Grid item justifyContent="center" alignContent="center" sx={{ mt: 1, mb: 1 }}>
+          <MapCreate setInput={setInput}
+            input={input} />
         </Grid>
         <Grid item xs={12} sm={6}>
-          {/*           <TextField
-            required
-            id="Localidad"
-            name="Localidad"
-            label="Localidad"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-          /> */}
           <FormControl sx={{ m: 1, minWidth: "12rem" }}>
             <InputLabel id="demo-simple-select-helper-label" color="secondary">Provincia</InputLabel>
             <Select
@@ -158,19 +127,12 @@ export default function Page1({ setInput, input }: InputProps) {
               color="secondary"
               onChange={handleChangeProvincia}>
               {allProvincias?.map((m, i) => (
-                <MenuItem value={m.id} key={i+1}>{m.nombre}</MenuItem>
+                <MenuItem value={m.id} key={i + 1}>{m.nombre}</MenuItem>
               ))}
             </Select>
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
-          {/*           <TextField
-            id="Provincia"
-            name="Provincia"
-            label="Provincia"
-            fullWidth
-            variant="standard"
-          /> */}
           <FormControl sx={{ m: 1, minWidth: "12rem" }}>
             <InputLabel id="demo-simple-select-helper-label" color="secondary">Localidad</InputLabel>
             <Select
@@ -183,36 +145,13 @@ export default function Page1({ setInput, input }: InputProps) {
               label="localidad"
               color="secondary"
               onChange={handleChangeSelect}>
-              {/* <MenuItem value=""><em>None</em></MenuItem> */}
               {allLocalidades?.map((m, i) => (
-                <MenuItem value={m.id} key={i+1}>{m.nombre}</MenuItem>
+                <MenuItem value={m.id} key={i + 1}>{m.nombre}</MenuItem>
               ))}
             </Select>
           </FormControl>
         </Grid>
-        {//DESPUES AGREGAMOS EL SELECT PARA PAISES
-        }
-        {/*         <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="País"
-            name="País"
-            label="País"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-          />
-        </Grid> */}
         <Grid item xs={12} sm={12}>
-          {/*           <TextField
-            required
-            id="Categoría"
-            name="Categoría"
-            label="Categoría"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-          /> */}
           <FormControl sx={{ m: 1, minWidth: "12rem" }}>
             <InputLabel id="demo-simple-select-helper-label" color="secondary">Categoria</InputLabel>
             <Select
@@ -224,14 +163,12 @@ export default function Page1({ setInput, input }: InputProps) {
               label="categoria"
               color="secondary"
               onChange={handleChangeCategoria}>
-              {/* <MenuItem value=""><em>None</em></MenuItem> */}
               {allCategorias?.map(m => (
                 <MenuItem value={m.id} key={m.id}>{m.categoria}</MenuItem>
               ))}
             </Select>
           </FormControl>
         </Grid>
-
         <Grid item xs={12} sm={6}>
           <TextField
             required
@@ -275,12 +212,6 @@ export default function Page1({ setInput, input }: InputProps) {
             onChange={handleChangeInput}
           />
         </Grid>
-        {/* <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
-        </Grid> */}
       </Grid>
     </React.Fragment>
   );
