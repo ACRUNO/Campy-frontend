0ccfeaaf570542a7c9bc65a6545830929a717d59
augusto2304 <augusto.alvarez2304@yahoo.com>
2022-12-30 13:01:43 -0300

    Prueba skeleton home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -21,6 +21,9 @@ import { useNavigate } from "react-router-dom";
 
 export default function Home() {
 
+   
+
+
     const dispatch: AppDispatch = useDispatch()
     const allProvincias: { id: number, nombre: string, imagen: string }[] = useSelector((state: RootState) => state.allProvincias)
 
@@ -28,14 +31,12 @@ export default function Home() {
 
     useEffect(() => {
         dispatch(getProvincias())
-        // dispatch(getAllCampings())
     }, [dispatch]);
 
 
 
     function handleClick(id: number) {
         dispatch(filterProvincia(id))
-        // dispatch(getCampingsProvincias())
         dispatch(filtrosPrincipales(id, 0, undefined, undefined))
         setTimeout(() => {
             navigate('/booking')
@@ -54,16 +55,15 @@ export default function Home() {
                         allProvincias?.map((e: { id: number, nombre: string, imagen: string }) => {
                             return (
                                 <Grid item className={s.item} sm={12} md={6} lg={4} xl={3} key={e.id}>
-                                    {/* <Link onClick={(event:MouseEvent<HTMLElement>) => handleClick(e.id)} to={`/booking/`} className={s.link}> */}
+
                                     <Box onClick={(event: MouseEvent<HTMLElement>) => handleClick(e.id)} sx={{cursor:"pointer"}}>
                                         <Cards id={e.id} name={e.nombre} img={e.imagen} />
                                     </Box>
-                                    {/* </Link> */}
                                 </Grid>
                             )
                         })
                     }
-                    <Loader open={allProvincias.length === 0} />
+                    {/* <Loader open={allProvincias.length === 0} /> */}
                 </Box>
             </Box>
             <Footer />
