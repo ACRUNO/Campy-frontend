3d810559e7b3424d707272bffe02a6448ed16ef0
GabrielaCarranza <gabicarranza2@gmail.com>
2022-12-30 15:32:45 -0300

    PRUEBA

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -8,7 +8,7 @@ export default function Banner() {
     const banner: string = "https://www.argentinosxargentina.ar/media/sticky/1920x830/zc/slide_2.jpg"
     return (
         <Box display='flex' justifyContent='center' alignItems='center'>
-            <Box position="absolute" color="primary.light" justifyContent='center' alignItems='center' >
+            <Box position="absolute" color="primary.light" justifyContent='center' alignItems='center'  >
                 <Typography variant="h1" display='flex' justifyContent='center' alignItems='center' sx={{ letterSpacing: 20 }}>ARGENTINA</Typography>
                 <Typography variant="h4" display='flex' justifyContent='center' textAlign='center' sx={{ letterSpacing: 5 }}>Pais multicultural con miles de rincones para descubrir</Typography>
             </Box>
