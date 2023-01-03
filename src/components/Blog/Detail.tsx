6d3e6e0a40e50dd7d6ee652a1a71f8f502b79287
Santiago Pagnanelli <109765245+SantiagoPag@users.noreply.github.com>
2023-01-03 10:42:07 -0300

    foto añadida al componente del detalle de los post del blog

--- src/components/Blog/Detail.tsx
+++ src/components/Blog/Detail.tsx
@@ -1,20 +1,21 @@
-import { TextField, Typography } from "@mui/material";
+import { TextField, Typography, Avatar } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
 
 interface PostDetail {
     id: number,
+    foto: string,
     username: string,
     fecha: string,
     titulo: string,
     texto: string,
     imagenes: Array<string>,
-    comentarios: { username: string, comentario: string, createdAt: string }[]
+    comentarios: { username: string, foto: string, comentario: string, createdAt: string }[]
 }
 
 export default function Detail(props: PostDetail) {
@@ -24,7 +25,7 @@ export default function Detail(props: PostDetail) {
             <Card sx={{ display: 'flex' }}>
                 <CardContent sx={{ flex: 1 }}>
                     <Typography component="h2" variant="h3">{props.titulo}</Typography>
-                    <Typography variant="subtitle1" color="text.secondary">{props.fecha} - {props.username}</Typography>
+                    <Typography variant="subtitle1" color="text.secondary">{props.fecha} - {props.username} <Avatar src={props.foto}/></Typography>
                     <Typography variant="h6" pb={3}>{props.texto}</Typography>
                     {props.imagenes?.map(e => (
                         <Box sx={{width: '100%'}} pt={1} component="img" src={e}></Box>
@@ -36,8 +37,14 @@ export default function Detail(props: PostDetail) {
                     <Typography variant="h6">Comentarios</Typography>
                     {props.comentarios?.map((e) => (
                         <Grid>
-                            <Typography>{new Date(e.createdAt).toLocaleDateString()}</Typography>
+                            <List>
+                            <Typography variant="subtitle1" color="text.secondary">{new Date(e.createdAt).toLocaleDateString()}</Typography>
+                            <Grid>
+                            <Avatar src={e.foto}/>
                             <Typography><strong>{e.username}</strong>: {e.comentario}</Typography>
+                            </Grid>
+                            <ListItem divider></ListItem>
+                            </List>
                         </Grid>
                     ))}
                 </CardContent>
