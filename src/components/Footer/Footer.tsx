64380bfd2a304e43a2bbb5bf39ded7a4382c4309
javier montenegro <inversiones.vvm@gmail.com>
2023-01-09 10:11:29 -0300

    about form y preguntas

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Grid } from '@mui/material';
-
+import { sendEmailContact } from "../../actions/SendEmail.action"
 import Link from '@mui/material/Link';
 import Style from "./footer.module.css"
 import { useNavigate } from "react-router-dom";
-
+import Faqs from "../AboutUs/Faqs"
+import Contact from "./Contact";
 
 function Copyright(props: any) {
 
@@ -16,7 +17,7 @@ function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="http://localhost:3000/">
+      <Link color="inherit">
         CAMPY S.A.
       </Link>{' '}
       {new Date().getFullYear()}
@@ -27,11 +28,28 @@ function Copyright(props: any) {
 
 
 export default function Footer() {
+
+
+
+
+
   let navigate: any = useNavigate();
 
   let handleAbout = () => {
     navigate("/aboutus")
     window.scrollTo(0, 0);
+  }
+
+  let maileo: any = {
+    name: "tito",
+    email: "algo",
+    subject: "todo mal",
+    text: "me cagaron mal la cama tenia piojos ",
+  }
+
+  let handleContact = async () => {
+    let roberto = await sendEmailContact(maileo)
+    console.log(roberto)
   }
 
   return (
@@ -56,17 +74,11 @@ export default function Footer() {
         </Grid>
 
         <Grid item xs={12} sm={12} md={4} justifyContent='center' alignItems='center' sx={{ p: 0.5 }}>
-          <Typography sx={{ cursor: "pointer" }}
-            variant="h5" component="h6"
-            textAlign='center'> Preguntas Frecuentes
-          </Typography>
+          <Faqs />
         </Grid>
 
         <Grid item xs={12} sm={12} md={4} justifyContent='center' alignItems='center' sx={{ p: 0.5 }}>
-          <Typography variant="h5" component="h6"
-            sx={{ cursor: "pointer" }}
-            textAlign='center'> Contacto
-          </Typography>
+          <Contact />
         </Grid>
       </Grid>
 
