fc1428ae72bd8c89dbbf299f49f657e2c172ea1e
Santiago Pagnanelli <109765245+SantiagoPag@users.noreply.github.com>
2023-01-10 16:38:34 -0300

    fotos agregadas al los post del blog

--- src/components/Blog/FeaturedPost.tsx
+++ src/components/Blog/FeaturedPost.tsx
@@ -5,7 +5,7 @@ import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import { useNavigate } from 'react-router-dom';
-import { Avatar } from '@mui/material';
+import { Avatar, Box, CardMedia } from '@mui/material';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import { useDispatch } from 'react-redux';
@@ -25,13 +25,13 @@ interface FeaturedPostProps {
   vistas: number,
   tipo: string
   userNow: number,
-  userId: number
+  userId: number,
+  imagenes: Array<string>
 }
 
 export default function FeaturedPost(props: FeaturedPostProps) {
   const navigate = useNavigate()
   const dispatch: AppDispatch = useDispatch()
-
   const handleClick = () => {
 
     if (props.userNow === props.userId) { dispatch(actions.cambiarComentariosVistos(props.id)) }
@@ -44,10 +44,20 @@ export default function FeaturedPost(props: FeaturedPostProps) {
 
   }
 
+
   return (
     <Grid item xs={12} md={10} onClick={() => handleClick()}>
+      
+
       <CardActionArea component="a" href="#" sx={{ "&:hover": { boxShadow: "0px 4px 8px rgba(50, 50, 50, 1)" } }}>
+      
         <Card sx={{ display: 'flex' }}>
+        {props.imagenes[0] ? <CardMedia
+                component="img"
+                image={props.imagenes[0]}
+                alt="img"
+                sx={{objectFit: "inherit", width: "30%", height: "19rem", pr: "1rem" }}
+              /> : null}
           <CardContent sx={{ flex: 1 }}>
             <Typography component="h2" variant="h5">
               {props.title}
@@ -64,9 +74,12 @@ export default function FeaturedPost(props: FeaturedPostProps) {
               </Typography>
             </Grid>
 
-            <Typography sx={{ mb: 1, mt: 1 }} variant="subtitle1" paragraph>
-              {props.description.length > 250 ? props.description.slice(0, 250) + "..." : props.description}
-            </Typography>
+            <Grid sx={{ display: "flex" }}>
+              <Typography sx={{ mb: 1, mt: 1 }} variant="subtitle1" paragraph>
+                {props.description.length > 250 ? props.description.slice(0, 250) + "..." : props.description}
+              </Typography>
+            </Grid>
+
             <Typography variant="subtitle1" color="secondary">
               Continuar leyendo...
             </Typography>
