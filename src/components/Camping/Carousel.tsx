1e5fcc268796cea998e72d5f45e26d5c2ad0dfd5
javier montenegro <inversiones.vvm@gmail.com>
2023-01-11 09:52:42 -0300

    carousel fix 2

--- src/components/Camping/Carousel.tsx
+++ src/components/Camping/Carousel.tsx
@@ -5,12 +5,11 @@ import Style from './Carousel.module.css'
 import foto2 from './portada.jpg'
 import foto3 from './comilona.webp' */
 import { useSelector } from "react-redux";
-
+import Box from "@mui/system/Box/Box";
 
 export default function Carousel() {
   let camp = useSelector((state: any) => state.detailCamping)
   const colors = camp?.imagenes;
-
   const delay = 5000;
   const timeoutRef: any = React.useRef(null);
 
@@ -36,25 +35,27 @@ export default function Carousel() {
   }, [index]);
 
   return (
-
-    <div className={Style.slideshow}>
-      {/* IMAGEN */}
-      <div className={Style.slideshowSlider}
-        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
-        {colors?.map((backgroundColor: any, index: any) => (
-          <img className={Style.slide} key={index} src={backgroundColor} />
-        ))}
-      </div>
-      <div className={Style.slideshowDots}>
-        {colors?.map((_: any, idx: any | number | ((prevState: number) => number) | null | undefined) => (
-          <div key={idx}
-            className={`${Style.slideshowDot}${index === idx ? " active" : ""}`}
-            onClick={() => {
-              setIndex(idx);
-            }}></div>
-        ))}
-      </div>
-    </div>
+    <Box>
+      {colors?.length == 1 ? <img src={colors} className={Style.slide} /> :
+        <div className={Style.slideshow}>
+          {/* IMAGEN */}
+          <div className={Style.slideshowSlider}
+            style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+            {colors?.map((backgroundColor: any, index: any) => (
+              <img className={Style.slide} key={index} src={backgroundColor} />
+            ))}
+          </div>
+          <div className={Style.slideshowDots}>
+            {colors?.map((_: any, idx: any | number | ((prevState: number) => number) | null | undefined) => (
+              <div key={idx}
+                className={`${Style.slideshowDot}${index === idx ? " active" : ""}`}
+                onClick={() => {
+                  setIndex(idx);
+                }}></div>
+            ))}
+          </div>
+        </div>}
+    </Box>
 
 
   );
